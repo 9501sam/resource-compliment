c4a08d6e879b73b242a5a0ede1cb3e29ca9eba76
lab <lab@debian-BULLSEYE-live-builder-AMD64>
2022-03-31 16:57:11 +0800

    algo

--- rc.cpp
+++ rc.cpp
@@ -72,18 +72,37 @@ void resource_complement()
     sort(_cluster.begin(), _cluster.end(), cmp_node);
     sort(_tasks.begin(), _tasks.end(), cmp_task);
     for (int i = 0; i < NUM_NODE; i++) {
-
+        node *n = &_cluster[i];
+        while (!_tasks.empty()) {
+            task *t = &_tasks.front();
+            if ((n->cpu < t->cpu) || (n->mem < t->mem))
+                break;
+            n->cpu -= t->cpu;
+            n->mem -= t->mem;
+            _tasks.erase(_tasks.begin());
+        }
+        while (!_tasks.empty()) {
+            task *t = &_tasks.back();
+            if ((n->cpu < t->cpu) || (n->mem < t->mem))
+                break;
+            n->cpu -= t->cpu;
+            n->mem -= t->mem;
+            _tasks.pop_back();
+        }
+    }
+    for (int i = 0; i < NUM_NODE; i++) {
+        cout << "(" << _cluster[i].cpu << ", " << _cluster[i].mem << ")" << endl;
     }
 }
 
-void
-_resource_complement()
+void _resource_complement()
 {
     vector<node> _cluster(NUM_NODE);
     vector<task> _tasks(NUM_TASK);
     for (int i = 0; i < NUM_NODE; i++) {
         _cluster[i].cpu = cluster[i].cpu;
         _cluster[i].mem = cluster[i].mem;
+        _cluster[i].w = cluster[i].w;
     }
     for (int i = 0; i < NUM_TASK; i++) {
         _tasks[i].cpu = tasks[i].cpu;
@@ -91,6 +110,22 @@ _resource_complement()
     }
     sort(_cluster.begin(), _cluster.end(), cmp_node);
     sort(_tasks.begin(), _tasks.end(), cmp_task);
+
+    for (int i = 0; i < NUM_TASK; i++) {
+        task *t = &_tasks[i];
+        node *n = &_cluster.front();
+        if ((t->cpu > n->cpu) || (t->mem > t->mem))
+            continue;
+        n->cpu -= t->cpu;
+        n->mem -= t->mem;
+        n->w = n->cpu * n->mem;
+        sort(_cluster.begin(), _cluster.end(), cmp_node);
+    }
+    
+    cout << "---------------" << endl;
+    for (int i = 0; i < NUM_NODE; i++) {
+        cout << "(" << _cluster[i].cpu << ", " << _cluster[i].mem << ")" << endl;
+    }
 }
 
 int main()
